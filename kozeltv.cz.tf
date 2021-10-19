68bbbea41e73c778b5855f2101c18cb7d4cbad6d
Tomas Kozak <kozak@talko.cz>
2021-10-19 20:30:09 +0200

    feat: add TVs DNS records

--- kozeltv.cz.tf
+++ kozeltv.cz.tf
@@ -1,3 +1,19 @@
 resource "cloudflare_zone" "kozeltv_cz" {
   zone = "kozeltv.cz"
 }
+
+resource "cloudflare_record" "kozeltv_cz_ip_v4" {
+  zone_id = cloudflare_zone.kozeltv_cz.id
+  name    = "kozeltv.cz"
+  type    = "A"
+  value   = "89.221.215.36"
+  proxied = true
+}
+
+resource "cloudflare_record" "kozeltv_cz_ip_v6" {
+  zone_id = cloudflare_zone.kozeltv_cz.id
+  name    = "kozeltv.cz"
+  type    = "AAAA"
+  value   = "2a02:2b88:2:424::"
+  proxied = true
+}

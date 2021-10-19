9a9084bf9527aecbbb1cc2c089e72a497b77d371
Tomas Kozak <kozak@talko.cz>
2021-10-19 20:30:09 +0200

    feat: add TVs DNS records

--- lojzatv.cz.tf
+++ lojzatv.cz.tf
@@ -1,3 +1,19 @@
 resource "cloudflare_zone" "lojzatv_cz" {
   zone = "lojzatv.cz"
 }
+
+resource "cloudflare_record" "lojzatv_cz_ip_v4" {
+  zone_id = cloudflare_zone.lojzatv_cz.id
+  name    = "lojzatv.cz"
+  type    = "A"
+  value   = "89.221.215.36"
+  proxied = true
+}
+
+resource "cloudflare_record" "lojzatv_cz_ip_v6" {
+  zone_id = cloudflare_zone.lojzatv_cz.id
+  name    = "lojzatv.cz"
+  type    = "AAAA"
+  value   = "2a02:2b88:2:424::"
+  proxied = true
+}

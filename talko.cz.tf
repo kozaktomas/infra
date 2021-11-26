73205d675f5171e33ec6186874c8d80cdc3bd38e
Tomas Kozak <kozak@talko.cz>
2021-11-26 20:38:32 +0100

    chore: remove import IDs

--- talko.cz.tf
+++ talko.cz.tf
@@ -1,9 +1,7 @@
-// 88f39cacb5ad1001d7bea4599217044b
 resource "cloudflare_zone" "talko_cz" {
   zone = "talko.cz"
 }
 
-// terraform import cloudflare_record.talko_cz_ip_v4 88f39cacb5ad1001d7bea4599217044b/ff62aec6d0aa46982f22ffae0716ca32
 resource "cloudflare_record" "talko_cz_ip_v4" {
   zone_id = cloudflare_zone.talko_cz.id
   name    = "talko.cz"
@@ -12,7 +10,6 @@ resource "cloudflare_record" "talko_cz_ip_v4" {
   proxied = false
 }
 
-// terraform import cloudflare_record.talko_cz_ip_v6 88f39cacb5ad1001d7bea4599217044b/facb628394deaf5af5eaafbe0e7ab0d6
 resource "cloudflare_record" "talko_cz_ip_v6" {
   zone_id = cloudflare_zone.talko_cz.id
   name    = "talko.cz"
@@ -21,7 +18,6 @@ resource "cloudflare_record" "talko_cz_ip_v6" {
   proxied = false
 }
 
-// terraform import cloudflare_record.talko_cz_mx_1 88f39cacb5ad1001d7bea4599217044b/ec11849c60860a684c323aa1616e1e5f
 resource "cloudflare_record" "talko_cz_mx_1" {
   zone_id  = cloudflare_zone.talko_cz.id
   name     = "talko.cz"
@@ -31,7 +27,6 @@ resource "cloudflare_record" "talko_cz_mx_1" {
   proxied  = false
 }
 
-// terraform import cloudflare_record.talko_cz_mx_2 88f39cacb5ad1001d7bea4599217044b/4ef95e764a8a8f2887c94c4f21d74abe
 resource "cloudflare_record" "talko_cz_mx_2" {
   zone_id  = cloudflare_zone.talko_cz.id
   name     = "talko.cz"
@@ -41,7 +36,6 @@ resource "cloudflare_record" "talko_cz_mx_2" {
   proxied  = false
 }
 
-// terraform import cloudflare_record.talko_cz_spf 88f39cacb5ad1001d7bea4599217044b/47b080b2a226313c71d0cedea87fe206
 resource "cloudflare_record" "talko_cz_spf" {
   zone_id = cloudflare_zone.talko_cz.id
   name    = "talko.cz"
@@ -49,7 +43,6 @@ resource "cloudflare_record" "talko_cz_spf" {
   value   = "v=spf1 include:spf.seznam.cz ~all"
 }
 
-// terraform import cloudflare_record.talko_cz_dmarc 88f39cacb5ad1001d7bea4599217044b/271f604ccf4bb207f8c069532415d65c
 resource "cloudflare_record" "talko_cz_dmarc" {
   zone_id = cloudflare_zone.talko_cz.id
   name    = "_dmarc"

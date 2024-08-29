23266661b9686853aebf362b71fc5c5000790d7a
Tomas Kozak <kozak@talko.cz>
2024-08-29 10:56:45 +0200

    Destroy cloudrun for playground

--- google-iam-binding.tf
+++ google-iam-binding.tf
@@ -1,13 +1,3 @@
-# https://cloud.google.com/firestore/docs/security/iam
-resource "google_project_iam_binding" "gcp_firestore_admin" {
-  role    = "roles/datastore.user"
-  project = var.GOOGLE_PROJECT_ID
-  members = [
-    "serviceAccount:${google_service_account.lesempolem_backend.email}",
-    "serviceAccount:${google_service_account.playground_veselice.email}",
-  ]
-}
-
 resource "google_project_iam_binding" "cloud_run_admin" {
   role    = "roles/run.admin"
   project = var.GOOGLE_PROJECT_ID
